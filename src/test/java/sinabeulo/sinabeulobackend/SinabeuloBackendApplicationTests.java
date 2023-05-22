9c6ce0f987c730fb07bae189b666809807a28d44
kangcherryin <rkdfls1004@gmail.com>
2023-05-23 02:03:18 +0900

    Initial commit

---
+++ src/test/java/sinabeulo/sinabeulobackend/SinabeuloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sinabeulo.sinabeulobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinabeuloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
